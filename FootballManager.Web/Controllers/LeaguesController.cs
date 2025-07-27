112f11788082163f9a39e3531f29276c71c09739
AlexKubecka <kubeckaalex@gmail.com>
2025-07-26 22:37:52 -0400

    Added Simulate season button and base logic

--- FootballManager.Web/Controllers/LeaguesController.cs
+++ FootballManager.Web/Controllers/LeaguesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using FootballManager.Models;
 
 namespace FootballManager.Web.Controllers
@@ -47,6 +48,7 @@ namespace FootballManager.Web.Controllers
 
             var league = new League(leagueName, nation) { Id = id };
             league.Teams = _dbContext.Teams
+                .AsNoTracking()
                 .Where(t => t.LeagueName == leagueName)
                 .Select(t => new Team
                 {
@@ -57,11 +59,79 @@ namespace FootballManager.Web.Controllers
                     StadiumCapacity = t.StadiumCapacity,
                     Value = t.Value,
                     LeagueName = t.LeagueName,
+                    Wins = t.Wins,
+                    Draws = t.Draws,
+                    Losses = t.Losses,
+                    GoalsFor = t.GoalsFor,
+                    GoalsAgainst = t.GoalsAgainst,
                     Players = _dbContext.Players.Where(p => p.TeamId == t.Id).ToList()
                 })
                 .ToList();
 
             return View(league);
+        }
+
+        [HttpPost]
+        public IActionResult SimulateSeason(int leagueId)
+        {
+            // Get league name by id (index in list)
+            var leagueNames = _dbContext.Teams
+                .Select(t => t.LeagueName)
+                .Distinct()
+                .Where(n => !string.IsNullOrEmpty(n))
+                .ToList();
+            if (leagueId < 1 || leagueId > leagueNames.Count)
+                return NotFound();
+            var leagueName = leagueNames[leagueId - 1];
+
+            // Get all teams in this league
+            var teams = _dbContext.Teams.Where(t => t.LeagueName == leagueName).ToList();
+            var rand = new Random();
+
+            // Reset stats
+            foreach (var team in teams)
+            {
+                team.Wins = 0;
+                team.Draws = 0;
+                team.Losses = 0;
+                team.GoalsFor = 0;
+                team.GoalsAgainst = 0;
+            }
+
+            // Simulate each pair of teams playing each other twice (home/away)
+            for (int i = 0; i < teams.Count; i++)
+            {
+                for (int j = 0; j < teams.Count; j++)
+                {
+                    if (i == j) continue;
+                    var home = teams[i];
+                    var away = teams[j];
+                    int homeGoals = rand.Next(0, 5);
+                    int awayGoals = rand.Next(0, 5);
+                    home.GoalsFor += homeGoals;
+                    home.GoalsAgainst += awayGoals;
+                    away.GoalsFor += awayGoals;
+                    away.GoalsAgainst += homeGoals;
+                    if (homeGoals > awayGoals)
+                    {
+                        home.Wins++;
+                        away.Losses++;
+                    }
+                    else if (homeGoals < awayGoals)
+                    {
+                        away.Wins++;
+                        home.Losses++;
+                    }
+                    else
+                    {
+                        home.Draws++;
+                        away.Draws++;
+                    }
+                }
+            }
+
+            _dbContext.SaveChanges();
+            return RedirectToAction("Details", new { id = leagueId });
         }
     }
 }
